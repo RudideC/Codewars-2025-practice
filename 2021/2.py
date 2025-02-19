c5d047ceb175604627a1ca439287fb2b0e5949bf
RudideC <rudidecastrop@gmail.com>
2025-02-19 18:08:43 +0100

    2

---
+++ 2021/2.py
@@ -0,0 +1,8 @@
+msg = input()
+
+print("IDENTIFICATION DIVISION.")
+print("PROGRAM-ID. HELLO-WORLD.")
+print("* simple hello world program")
+print("PROCEDURE DIVISION.")
+print(f"    DISPLAY '{msg}'.")
+print("    STOP RUN.")
